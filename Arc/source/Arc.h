b50b8e347e464b2449ab846763cc86fbc4f931fc
YaMolod <skystep56@gmail.com>
2020-10-04 20:41:20 +0500

    test_ver

---
+++ Arc/source/Arc.h
@@ -0,0 +1,10 @@
+#pragma once
+
+// for applications only
+
+#include "Arc/Application.h"
+
+// ---Entry Point--------------------------
+#include "Arc/EntryPointh.h"
+// ---===========--------------------------
+
---
+++ Sandbox/source/main.cpp
@@ -0,0 +1,15 @@
+#include "Arc.h"
+
+class Sandbox : public Arc::Application
+{
+public:
+	Sandbox() {}
+	~Sandbox() {}
+
+
+};
+
+Arc::Application* Arc::CreateApplication()
+{
+	return new Sandbox();
+}
