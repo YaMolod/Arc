3ef03c0940989ceb08813c501dec1391ae7c7433
YaMolod <skystep56@gmail.com>
2020-12-06 17:34:59 +0500

    Shader modification

--- Arc/source/Arc/Renderer/Texture.cpp
+++ Arc/source/Arc/Renderer/Texture.cpp
@@ -6,12 +6,24 @@
 namespace ARC
 {
 
+	Ref<Texture2D> Texture2D::Create(uint32_t width, uint32_t height)
+	{
+		switch (Renderer::GetAPI())
+		{
+		case RendererAPI::API::NONE:		ARC_CORE_ASSERT(false, "RendererAPI::NONE is not supported!"); return nullptr;
+		case RendererAPI::API::OpenGL:		return CreateRef<OpenGLTexture2D>(width, height);
+		}
+
+		ARC_CORE_ASSERT(false, "Unknown RendererAPI!");
+		return nullptr;
+	}
+
 	Ref<Texture2D> Texture2D::Create(const std::string& path)
 	{
 		switch (Renderer::GetAPI())
 		{
 		case RendererAPI::API::NONE:		ARC_CORE_ASSERT(false, "RendererAPI::NONE is not supported!"); return nullptr;
-		case RendererAPI::API::OpenGL:		return std::make_shared<OpenGLTexture2D>(path);
+		case RendererAPI::API::OpenGL:		return CreateRef<OpenGLTexture2D>(path);
 		}
 
 		ARC_CORE_ASSERT(false, "Unknown RendererAPI!");
--- Sandbox/assets/shaders/Texture.glsl
+++ Sandbox/assets/shaders/Texture.glsl
@@ -23,8 +23,9 @@ layout(location = 0) out vec4 color;
 in vec2 v_TextureCoord;
 
 uniform sampler2D u_Texture;
+uniform vec4 u_Color;
 
 void main()
 {
-	color = texture(u_Texture, v_TextureCoord);
+	color = texture(u_Texture, v_TextureCoord * 10.0) * u_Color;
 }
